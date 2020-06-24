f27815a60cbfd1480439eb025e3c3c2fc161cdde
Roberto Bissanti <roberto.bissanti@gmail.com>
2020-06-24 23:01:13 +0200

    Update Spazio_Graphite_Plugin.php

--- Spazio_Graphite_Plugin.php
+++ Spazio_Graphite_Plugin.php
@@ -13,19 +13,7 @@
 /*define( 'Spazio_Graphite_Plugin_C', '1' );
 if( !defined( 'Spazio_Graphite_Plugin_C' ) ) {exit();};*/
 
-//function spazio_graphite_login_logo() { 
-//	//$logo_images = plugin_dir_url( __FILE__ ) . 'imgs/spazio_graphite_login_logo.png';
-//	$Content   = '<style type="text/css">';
-//	$Content  .= 'body.login div#login h1 a {';
-//	$Content  .= 'background-image: url(https://www.spaziographite.it/wp-content/plugins/Spazio_Graphite_Plugin/imgs/spazio_graphite_login_logo.png);';
-//	$Content  .= 'padding-bottom: 30px;';
-//	$Content  .= '}'; 
-//	$Content  .= '</style>';
-//	return $Content;
-//} 
-//add_action( 'login_enqueue_scripts', 'spazio_graphite_login_logo' );
-//add_action( 'admin_enqueue_scripts', 'spazio_graphite_login_logo' );
-//add_action( 'wp_enqueue_scripts', 'spazio_graphite_login_logo' );
+
 
 //* Personalizzare l'immagine nella pagina di login 
   function custom_loginlogo() { echo '<style type="text/css"> h1 a {background-image: url(https://www.spaziographite.it/wp-content/plugins/Spazio_Graphite_Plugin/imgs/spazio_graphite_login_logo.png) !important; } </style>'; } add_action('login_head', 'custom_loginlogo'); 
@@ -35,6 +23,12 @@ if( !defined( 'Spazio_Graphite_Plugin_C' ) ) {exit();};*/
 //* Personalizzare title tag image 
   function custom_login_title() { return get_option( 'blogname' ); } add_filter( 'login_headertext', 'custom_login_title' );
 
+
+function remove_wordpress_admin_logo() {
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('wp-logo');
+}
+add_action('wp_before_admin_bar_render', 'remove_wordpress_admin_logo', 0);
 
 
 ?>
