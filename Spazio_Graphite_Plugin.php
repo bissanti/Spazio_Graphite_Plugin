95132daaf66203de34515bb3d3b7fe6b47c28409
Roberto Bissanti <roberto.bissanti@gmail.com>
2020-06-25 01:15:50 +0200

    Update Spazio_Graphite_Plugin.php

--- Spazio_Graphite_Plugin.php
+++ Spazio_Graphite_Plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Spazio_Graphite_Plugin
  * Plugin URI: https://github.com/bissanti/Spazio_Graphite_Plugin
  * Description: A Spazio Graphite Plugins collection
- * Version: 1.0.5
+ * Version: 1.0.4
  * Author: Roberto Bissanti
  * Author URI: https://studiobissanti.com
  * License: GPL2
@@ -65,7 +65,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
 		$config = array(
 			'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
 			'proper_folder_name' => 'Spazio_Graphite_Plugin', // this is the name of the folder your plugin lives in
-			'api_url' => 'https://api.github.com/repos/bissanti/Spazio_Graphite_Plugin', // the GitHub API url of your GitHub repo
+			'api_url' => 'https://api.github.com/bissanti/repos', // the GitHub API url of your GitHub repo
 			'raw_url' => 'https://raw.github.com/bissanti/Spazio_Graphite_Plugin/master', // the GitHub raw url of your GitHub repo
 			'github_url' => 'https://github.com/bissanti/Spazio_Graphite_Plugin', // the GitHub url of your GitHub repo
 			'zip_url' => 'https://github.com/bissanti/Spazio_Graphite_Plugin/zipball/master', // the zip url of the GitHub repo
@@ -73,7 +73,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
 			'requires' => '3.0', // which version of WordPress does your plugin require?
 			'tested' => '3.3', // which version of WordPress is your plugin tested up to?
 			'readme' => 'README.md', // which file to use as the readme for the version number
-			'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+			'access_token' => '77d5e10bad2e6a09dc647695147f6d230c28a3f8', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
 		);
 		new WP_GitHub_Updater($config);
 	}
