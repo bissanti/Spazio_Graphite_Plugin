c8f479ab1a2c833fdaf84e60b6b73ee06fef2d4b
Roberto Bissanti <roberto.bissanti@gmail.com>
2020-06-25 00:21:45 +0200

    Updater with Github

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Spazio_Graphite_Plugins
+`~Current Version:1.0.3~`
  
--- Spazio_Graphite_Plugin.php
+++ Spazio_Graphite_Plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Spazio_Graphite_Plugin
  * Plugin URI: https://github.com/bissanti/Spazio_Graphite_Plugin
  * Description: A Spazio Graphite Plugins collection
- * Version: 0.1.2
+ * Version: 1.0.3
  * Author: Roberto Bissanti
  * Author URI: https://studiobissanti.com
  * License: GPL2
@@ -39,7 +39,7 @@ if( !defined( 'Spazio_Graphite_Plugin_C' ) ) {exit();};*/
 //add_action( 'login_enqueue_scripts', 'spazio_graphite_login_logo' );
 //add_action( 'admin_enqueue_scripts', 'spazio_graphite_login_logo' );
 //add_action( 'wp_enqueue_scripts', 'spazio_graphite_login_logo' );
-
+include_once('updater.php');
 //* Personalizzare l'immagine nella pagina di login 
   function custom_loginlogo() { 
   	echo '<style type="text/css"> h1 a {background-image: url(';
@@ -59,6 +59,26 @@ function remove_wordpress_admin_logo() {
     $wp_admin_bar->remove_menu('wp-logo');
 }
 add_action('wp_before_admin_bar_render', 'remove_wordpress_admin_logo', 0);
+
+
+if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+		$config = array(
+			'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+			'proper_folder_name' => 'Spazio_Graphite_Plugin', // this is the name of the folder your plugin lives in
+			'api_url' => 'https://api.github.com/repos/bissanti/Spazio_Graphite_Plugin', // the GitHub API url of your GitHub repo
+			'raw_url' => 'https://raw.github.com/bissanti/Spazio_Graphite_Plugin/master', // the GitHub raw url of your GitHub repo
+			'github_url' => 'https://github.com/bissanti/Spazio_Graphite_Plugin', // the GitHub url of your GitHub repo
+			'zip_url' => 'https://github.com/bissanti/Spazio_Graphite_Plugin/zipball/master', // the zip url of the GitHub repo
+			'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+			'requires' => '3.0', // which version of WordPress does your plugin require?
+			'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+			'readme' => 'README.md', // which file to use as the readme for the version number
+			'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+		);
+		new WP_GitHub_Updater($config);
+	}
+
+
 
 
 ?>
