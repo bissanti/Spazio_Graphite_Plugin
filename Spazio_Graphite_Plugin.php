8c8dfcde1df3cf591aafb761b59d11b579e55568
Roberto Bissanti <roberto.bissanti@gmail.com>
2020-06-24 09:23:49 +0200

    Create Spazio_Graphite_Plugin.php

---
+++ Spazio_Graphite_Plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Spazio_Graphite_Plugin
+ * Description: A Spazio-graphite 
+ * Version: 0.1.0
+ * Author: Roberto Bissanti
+ * Author URI: https://studiobissanti.com
+ *
+ * @package Spazio_Graphite_Plugin
+ */
+
+
+?>
