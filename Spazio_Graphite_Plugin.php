018c6d43e8de99b674972119569e7d0bd30272c4
Roberto Bissanti <roberto.bissanti@gmail.com>
2020-06-28 09:38:42 +0200

    update test2

--- Spazio_Graphite_Plugin.php
+++ Spazio_Graphite_Plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Spazio_Graphite_Plugin
  * Plugin URI: https://github.com/bissanti/Spazio_Graphite_Plugin
  * Description: A Spazio Graphite Plugins collection
- * Version: 1.0.5
+ * Version: 1.0.6
  * Author: Roberto Bissanti
  * Author URI: https://studiobissanti.com
  * License: GPL2
@@ -26,20 +26,30 @@
 /*define( 'Spazio_Graphite_Plugin_C', '1' );
 if( !defined( 'Spazio_Graphite_Plugin_C' ) ) {exit();};*/
 
-//function spazio_graphite_login_logo() { 
-//	//$logo_images = plugin_dir_url( __FILE__ ) . 'imgs/spazio_graphite_login_logo.png';
-//	$Content   = '<style type="text/css">';
-//	$Content  .= 'body.login div#login h1 a {';
-//	$Content  .= 'background-image: url(https://www.spaziographite.it/wp-content/plugins/Spazio_Graphite_Plugin/imgs/spazio_graphite_login_logo.png);';
-//	$Content  .= 'padding-bottom: 30px;';
-//	$Content  .= '}'; 
-//	$Content  .= '</style>';
-//	return $Content;
-//} 
-//add_action( 'login_enqueue_scripts', 'spazio_graphite_login_logo' );
-//add_action( 'admin_enqueue_scripts', 'spazio_graphite_login_logo' );
-//add_action( 'wp_enqueue_scripts', 'spazio_graphite_login_logo' );
-include_once( dirname( __FILE__ ) .'/updater.php');
+
+include_once( 'inc/updater.php');
+/*github updater*/
+if (is_admin()) {
+$config = array( 
+		'slug' => plugin_basename(__FILE__), // l'abbreviazione del plugin 
+		'proper_folder_name' => 'Spazio_Graphite_Plugin', // il nome della cartella che contiene il nostro plugin 
+		'api_url' => 'http://api.github.com/repos/bissanti/Spazio_Graphite_Plugin', // la GitHub API url del repository contenente il plugin 
+		'raw_url' => 'http://raw.github.com/bissanti/Spazio_Graphite_Plugin/master', // la GitHub raw url del repository contenente il plugin 
+		'github_url' => 'http://github.com/bissanti/Spazio_Graphite_Plugin', // la GitHub url del repository contenente il plugin 
+		'zip_url' => 'http://github.com/bissanti/Spazio_Graphite_Plugin/zipball/master', // dove si trova l'archivio .zip del repository 
+		'sslverify' => true, // se WordPress deve utilizzare un certificato SSL quando effettua il controllo sull'aggiornamento 
+		'requires' => '3.0', // specificare quale versione di WordPress e' richiesta da questo plugin 
+		'tested' => '3.3', // fino a che versione di WordPress hai testato il plugin? 
+		'readme' => 'README.md', // quale file deve essere usato per controllare la versione del plugin? 
+		'access_token' => '', // serve soltanto quando utilizziamo repository WordPress privati 
+	); 
+
+	new WP_GitHub_Updater($config); 
+}
+/*end github updater*/
+
+
+
 //* Personalizzare l'immagine nella pagina di login 
   function custom_loginlogo() { 
   	echo '<style type="text/css"> h1 a {background-image: url(';
@@ -60,23 +70,6 @@ function remove_wordpress_admin_logo() {
 }
 add_action('wp_before_admin_bar_render', 'remove_wordpress_admin_logo', 0);
 
-
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-		$config = array(
-			'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-			'proper_folder_name' => 'Spazio_Graphite_Plugin', // this is the name of the folder your plugin lives in
-			'api_url' => 'https://api.github.com/repos/bissanti/Spazio_Graphite_Plugin', // the GitHub API url of your GitHub repo
-			'raw_url' => 'https://raw.github.com/bissanti/Spazio_Graphite_Plugin/master', // the GitHub raw url of your GitHub repo
-			'github_url' => 'https://github.com/bissanti/Spazio_Graphite_Plugin', // the GitHub url of your GitHub repo
-			'zip_url' => 'https://github.com/bissanti/Spazio_Graphite_Plugin/zipball/master', // the zip url of the GitHub repo
-			'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-			'requires' => '3.0', // which version of WordPress does your plugin require?
-			'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-			'readme' => 'README.md', // which file to use as the readme for the version number
-			'access_token' => '77d5e10bad2e6a09dc647695147f6d230c28a3f8', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
-		);
-		new WP_GitHub_Updater($config);
-	}
 
 
 
